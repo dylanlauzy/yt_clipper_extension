5c462f60ec194e52239cf8171ee2bdee08bd0441
dylanlauzy <dylzylau@gmail.com>
2022-10-19 10:07:54 -0700

    Re-factored code and introduced core functionality
    background.js now has listeners that message contentScript.js
    
    contentScript.js is injected into YouTube.com/watch sites
    
    Re-implemented functionality of:
    - Saving video info upon button press to chrome.storage.sync
    - Generating popup with basic UI
    - Taking input for notes and storing to chrome.storage.sync
    - Deleting notes/move video to timestamp buttons

--- background.js
+++ background.js
@@ -1,131 +1,35 @@
-// Functions
-function sendMessage(tab, message) {
-  chrome.tabs.sendMessage(tab, msg = {txt: message})
+async function getCurrentTab() {
+  let queryOptions = { active: true, lastFocusedWindow: true };
+  // `tab` will either be a `tabs.Tab` instance or `undefined`.
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
 }
 
-async function executeScript(tab, script, files=0) {
-// injecting script into a tab (for files, pass array of strings; for inline function, pass function)
-  if(files) {
-    await chrome.scripting.executeScript({
-      target: {tabId: tab},
-      files: script
-    })
-  }
-  else {
-    await chrome.scripting.executeScript({
-      target: {tabId: tab},
-      function: script
-    })
-  }
-}
-
-// Executable Content Functions
-async function storeVideoInfo() {
-  let video_id = window.location.href.match(/(\?|&)v=[^&]+/)[0].slice(3)
-  let channelName = document.querySelector("#upload-info a")
-  let videoTitle = document.querySelector("#info-contents h1")
-  let today = new Date()
-  
-  console.log("retrieving video info for video id: " + video_id)
-
-  let videoData = {}
-  videoData[video_id] = {
-    "video_title": videoTitle.innerText,
-    "channel_name": channelName.innerText,
-    "time_saved": {
-      "timezone": today.getTimezoneOffset(),
-      "time": today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds(),
-      "day": today.getDate(),
-      "month": today.getMonth() + 1,
-      "year": today.getFullYear(),
-    },
-    "notes": []
-  }
-  await chrome.storage.sync.set(videoData)
-  await chrome.storage.sync.get([video_id], (data) => {
-    console.log("video info stored successfully\n","data saved to storage:", data)
-  })
-}
-
-async function togglePopup() {
-  let video_id = window.location.href.match(/(\?|&)v=[^&]+/)[0].slice(3)
-  let videoPlayer = document.querySelector("video.video-stream")
-  let popupElem = document.querySelector(".ytclipper-popup-container")
-  if (popupElem) {
-    popupElem.remove()
-  }
-  else {
-    await chrome.storage.sync.get([video_id], (data) => {
-      console.log("Generating popup...\n Data Retrieved:", data)
-      
-      let newPopup = document.createElement("div")
-      newPopup.classList.add("ytclipper-popup-container")
-      newPopup.innerHTML =`<div class="popup-title">${data[video_id].video_title}</div><textarea name="" class="note-input" placeholder="start typing..."></textarea><div class="footer flex-container"><div class="toolbar"><ul class="tools flex-container"><li class="tool-icon-wrap"><button class="tool-icon-button bold-icon">B</button></li><li class="tool-icon-wrap"><button class="tool-icon-button italic-icon">I</button></li><li class="tool-icon-wrap"><button class="tool-icon-button strikethrough-icon">S</button></li><li class="tool-icon-wrap"><button class="tool-icon-button code-icon">C</button></li><li class="tool-icon-wrap"><button class="tool-icon-button link-icon">L</button></li><li class="tool-icon-wrap"><button class="tool-icon-button ol-icon">1.</button></li><li class="tool-icon-wrap"><button class="tool-icon-button ul-icon">&#8226;</button></li><li class="tool-icon-wrap"><button class="tool-icon-button quote-icon">"</button></li><li class="tool-icon-wrap"><button class="tool-icon-button codeblock-icon">[]</button></li></ul></div><button class="enter-btn">enter</button></div>`
-      document.body.appendChild(newPopup)
-      console.log("popup generated")
-      
-      let textareaElem = document.querySelector(".note-input")
-      let enterBtn = document.querySelector(".enter-btn")
-      enterBtn.addEventListener("click", () => {
-        console.log("enter clicked")
-        let noteInput = textareaElem.value
-        if(noteInput != "") {
-          let noteData = {}
-          let regExp = /.+(?=\n+)/
-          noteData.time_stamp = Math.floor(videoPlayer.currentTime)
-          if(regExp.test(noteInput)) {
-            noteData.header = noteInput.match(regExp)[0].trim()
-            noteData.content = noteInput.replace(regExp, "").trim()
-          }
-          else {
-            noteData.content = noteInput
-          }
-          data[video_id].notes.push(noteData)
-          chrome.storage.sync.set(data)
-          console.log("note saved:\n", noteData)
-          let popupElem = document.querySelector(".ytclipper-popup-container")
-          popupElem.remove()
-        }
-      })
-      textareaElem.focus()
-    })
-  }
-}
-
-// Listeners
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.clear()
-  console.log('YouTube clipper extension installed and sync data cleared')
 })
 
-chrome.action.onClicked.addListener(async (tab) => {
-  await chrome.storage.sync.get(null, (data) => {console.log("extension button clicked\ncurrent data:", data)})
-  
-  let video_id = tab.url.match(/(\?|&)v=[^&]+/)[0].slice(3)
-  let regExp = new RegExp("^https:\/\/www.youtube.com\/watch")
-  
-  if(regExp.test(tab.url)) {
-    console.log("this is a YouTube video")
-    chrome.storage.sync.get([video_id], async (data) => {
-      if(!Object.keys(data).length) {
-        console.log("this video's notes aren't in storage\n saving details...")
-        sendMessage(tab.id, "save")
-        await executeScript(tab.id, storeVideoInfo)
-        chrome.storage.sync.get([video_id], (result) => {console.log("video info stored successfully\n","data saved to storage:", result)})
-      }
-      else {
-        console.log(`video already saved in storage, data retrieved:\n`, data)
-      }
-      await executeScript(tab.id, togglePopup)
-      chrome.scripting.insertCSS({
-        target: {tabId: tab.id},
-        files: ["./popup/style/style.css"]
-      })
-      console.log("generating popup")
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  console.log(changeInfo, tab)
+  if(changeInfo.status == "complete" && tab.url) {
+    let urlParams
+    if (tab.url.includes("youtube.com/watch")) {
+      urlParams = new URLSearchParams(tab.url.split("?")[1])
+    }
 
+    chrome.tabs.sendMessage(tabId, {
+      type: "LINK_LOADED",
+      videoId: urlParams ? urlParams.get("v") : 0
     })
   }
-  else {
-    console.log("not a YouTube video")
-  }
+})
+
+// Not working
+chrome.commands.onCommand.addListener(async (command) => {
+  console.log("shortcut pressed", command)
+  const currentTab = await getCurrentTab()
+  
+  chrome.tabs.sendMessage(currentTab.id, {
+    type: "EXTENSION_KEY_PRESSED"
+  })
 })
---
+++ contentScript.js
@@ -0,0 +1,196 @@
+// Variables
+let currentVideo = "";
+let currentVideoInfo = {};
+let youtubeLeftControls, youtubeVideoPlayer, titleElem, channelElem;
+
+// Message Responses
+chrome.runtime.onMessage.addListener((obj, sender, response) => {
+  const {type, videoId} = obj;
+
+  if(type === "LINK_LOADED") {
+    console.log("link loaded")
+    const popupExists = document.getElementsByClassName("ytclipper-popup-container")[0]
+    
+    if (popupExists) {
+      // prevents popup remaining when navigating between YouTube videos
+      popupExists.remove()
+    }
+
+    if (videoId) {
+      currentVideo = videoId
+      videoLoaded()
+    }
+  }
+
+  if(type === "EXTENSION_KEY_PRESSED") {
+    togglePopup()
+  }
+})
+
+// Handlers
+
+const videoLoaded = async () => {
+  const noteBtnExists = document.getElementsByClassName("note-btn")[0];
+
+  if (!noteBtnExists) {
+      const noteBtn = document.createElement("img");
+      noteBtn.src = chrome.runtime.getURL("assets/bookmark.png");
+      noteBtn.className = "ytp-button " + "note-btn";
+      noteBtn.title = "click to start taking notes";
+
+      youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
+      youtubeVideoPlayer = document.getElementsByClassName("video-stream")[0];
+
+      youtubeLeftControls.appendChild(noteBtn);
+
+      noteBtn.addEventListener("click", togglePopup);
+  }
+}
+
+const fetchVideoData = async () => {
+  titleElem = document.querySelector("#info-contents h1 yt-formatted-string")
+  channelElem = document.querySelector("#channel-name a")
+
+  return new Promise((resolve) => {
+    chrome.storage.sync.get([currentVideo], (obj) => {
+        resolve(obj[currentVideo] ? JSON.parse(obj[currentVideo]): {
+          videoTitle: titleElem.textContent,
+          videoChannel: channelElem.textContent,
+          firstSaved: new Date(),
+          notes: []
+        })
+    })
+  })
+}
+
+
+const editNote = () => {
+  
+}
+
+const deleteNote = () => {
+  
+}
+
+const togglePopup = async () => {
+  currentVideoInfo = await fetchVideoData()
+  console.log(currentVideoInfo)
+  
+  const popupExists = document.getElementsByClassName("ytclipper-popup-container")[0]
+  
+  if (popupExists) {
+    console.log("removing popup")
+    popupExists.remove()
+  } else {
+    console.log("generating popup...")
+    generatePopup()
+  }
+}
+
+const generatePopup = () => {
+  const documentBody = document.getElementsByTagName("body")[0];
+  
+  const containerElem = document.createElement("div");
+  const titleElem = document.createElement("h1");
+  const channelElem = document.createElement("div");
+  const formElem = document.createElement("form");
+  const noteInputElem = document.createElement("input");
+  const submitElem = document.createElement("button");
+  const submitIconElem = document.createElement("img")
+  
+  containerElem.className = "ytclipper-popup-container"
+  
+  titleElem.className = "popup-title"
+  titleElem.textContent = currentVideoInfo.videoTitle
+  
+  channelElem.textContent = currentVideoInfo.videoChannel
+  
+  noteInputElem.type = "text"
+  noteInputElem.className = "note-input"
+  
+  submitIconElem.src = chrome.runtime.getURL("/assets/play.png")
+  
+  submitElem.appendChild(submitIconElem)
+  
+  formElem.appendChild(noteInputElem)
+  formElem.appendChild(submitElem)
+  formElem.addEventListener("submit", newNote)
+
+  
+  containerElem.appendChild(titleElem)
+  containerElem.appendChild(channelElem)
+  containerElem.appendChild(formElem)
+  documentBody.appendChild(containerElem)
+
+  for(i in currentVideoInfo.notes) {
+    addNoteElement(containerElem, currentVideoInfo.notes[i])
+  }
+
+  noteInputElem.focus()
+}
+
+const newNote = (event) => {
+  event.preventDefault()
+  
+  const timestamp = youtubeVideoPlayer.currentTime
+  const noteInputElem = document.getElementsByClassName("note-input")[0]
+  const newNote = {
+    timestamp: timestamp,
+    noteBody: noteInputElem.value,
+    firstCreated: new Date()
+  }
+
+  currentVideoInfo["notes"] = [...currentVideoInfo["notes"], newNote]
+  chrome.storage.sync.set({[currentVideo]: JSON.stringify(currentVideoInfo)})
+  noteInputElem.value = ""
+  addNoteElement(noteInputElem.parentNode.parentNode, newNote)
+}
+
+const addNoteElement = (parentElem, note) => {
+  const noteContainerElem = document.createElement("div");
+  const noteBodyElem = document.createElement("div");
+  const noteControlsElem = document.createElement("div");
+
+  noteBodyElem.className = "note-body";
+  noteBodyElem.textContent = `${getTime(note.timestamp)} - ${note.noteBody}`
+
+  noteControlsElem.className = "note-controls";
+  setBookmarkAttributes("play", onPlay, noteControlsElem);
+  setBookmarkAttributes("delete", onDelete, noteControlsElem);
+
+  noteContainerElem.id = "note-" + note.timestamp;
+  noteContainerElem.className = "note";
+  noteContainerElem.setAttribute("timestamp", note.timestamp);
+  
+  noteContainerElem.appendChild(noteBodyElem);
+  noteContainerElem.appendChild(noteControlsElem);
+  parentElem.appendChild(noteContainerElem);
+}
+
+const onPlay = async e => {
+  youtubeVideoPlayer.currentTime = e.target.parentNode.parentNode.getAttribute("timestamp")
+};
+
+const onDelete = async e => {
+  const noteToDelete = e.target.parentNode.parentNode
+  const noteTimestamp = noteToDelete.getAttribute("timestsamp")
+  noteToDelete.remove()
+
+  currentVideoInfo["notes"] = currentVideoInfo["notes"].filter((note) => {note.timestamp != noteTimestamp})
+  chrome.storage.sync.set({[currentVideo]: JSON.stringify(currentVideoInfo)})
+};
+
+const setBookmarkAttributes =  (src, eventListener, controlParentElem) => {
+  const controlElem = document.createElement("img");
+  controlElem.src = chrome.runtime.getURL("/assets/" + src + ".png")
+  controlElem.title = src;
+  controlElem.addEventListener("click", eventListener);
+  controlParentElem.appendChild(controlElem);
+};
+
+const getTime = (seconds) => {
+  let date = new Date(0);
+  date.setSeconds(seconds);
+  
+  return seconds >= 3600 ? date.toISOString().substring(11, 19) : date.toISOString().substring(14, 19);
+}
