c38fcafe6a581751f90d2ac39eb698756474761b
Dylan Lau <dylzylau@gmail.com>
2022-11-29 00:45:04 -0800

    Added props for Header and Popup

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 import { FaVideo } from "react-icons/fa"
 
-const Header = () => {
+const Header = ({ title, channelName}) => {
   return (
     <div className="rounded-2xl bg-slate-200 px-4 py-2">
       <div className="flex gap-x-4">
@@ -8,14 +8,17 @@ const Header = () => {
           <FaVideo className="w-6 fill-emerald-500" />
         </div>
         <div>
-          <h1 className="text-xl font-bold">All-In Podcast</h1>
-          <h2 className="text-sm font-medium">
-            Get the Hornets commentator a glass of water 😂
-          </h2>
+          <h1 className="text-xl font-bold">{channelName}</h1>
+          <h2 className="text-sm font-medium">{title}</h2>
         </div>
       </div>
     </div>
   )
+}
+
+Header.defaultProps = {
+  title: "",
+  channelName: ""
 }
 
 export default Header
--- components/Popup.js
+++ components/Popup.js
@@ -12,9 +12,9 @@ const Popup = () => {
   return (
     <div className="ytclipper-popup-container rounded-2xl px-4 py-2 flex flex-col gap-y-2">
       <Logo />
-      <Header />
+      <Header title={videoTitle} channelName={videoChannel}/>
       <NoteInput />
-      <Notes />
+      <Notes notes={notes} />
       <ExportBtn />
     </div>
   )
